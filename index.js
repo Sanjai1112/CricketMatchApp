b8f78eac4cc12e07c98054cdb86af4f098938bd8
Sanjai <sanjaikumar4773@gmail.com>
2019-04-25 12:28:17 +0530

    added cricapi

--- index.js
+++ index.js
@@ -1,12 +1,66 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const http = require("http");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
+});
+app.get("/matches", (request, response) => {
+  // http.get(
+  //   "http://cricapi.com/api/matches?apikey=ceTb1jjmV4PZCnj6TBjkuOgu26W2",
+  //   recData => {
+  //     console.log(recData);
+  //     res.redirect("/");
+  //   }
+  // );
+  http
+    .get(
+      "http://cricapi.com/api/matches?apikey=ceTb1jjmV4PZCnj6TBjkuOgu26W2",
+      res => {
+        const { statusCode } = res;
+        const contentType = res.headers["content-type"];
+        let error;
+        if (statusCode !== 200) {
+          error = new Error("Request Failed.\n" + `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+          error = new Error(
+            "Invalid content-type.\n" +
+              `Expected application/json but received ${contentType}`
+          );
+        }
+        if (error) {
+          console.error(error.message);
+          // Consume response data to free up memory
+          res.resume();
+          return;
+        }
+
+        res.setEncoding("utf8");
+        let rawData = "";
+        res.on("data", chunk => {
+          rawData += chunk;
+        });
+
+        res.on("end", () => {
+          try {
+            const parsedData = JSON.parse(rawData);
+            // console.log(parsedData);
+            response.render("showMatch", { matches: parsedData.matches });
+          } catch (e) {
+            console.error(e.message);
+            response.send(e.message);
+          }
+        });
+      }
+    )
+    .on("error", e => {
+      console.error(`Got error: ${e.message}`);
+      response.send(e.message);
+    });
 });
 
 const port = process.env.PORT || 3001;
