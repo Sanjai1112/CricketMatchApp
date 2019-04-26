02e8798b6d4683333229b98d32307f812405a900
Sanjai <sanjaikumar4773@gmail.com>
2019-04-26 18:07:24 +0530

    added cricapi call middleware and view match

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const http = require("http");
 const path = require("path");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,21 +12,10 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.get("/matches", (request, response) => {
-  // http.get(
-  //   "http://cricapi.com/api/matches?apikey=ceTb1jjmV4PZCnj6TBjkuOgu26W2",
-  //   recData => {
-  //     console.log(recData);
-  //     res.redirect("/");
-  //   }
-  // );
-  http
-    .get(
-      "http://cricapi.com/api/matches?apikey=ceTb1jjmV4PZCnj6TBjkuOgu26W2",
-      res => {
+let cricapiCall = url => {
+  return new Promise((resolve, reject) => {
+    http
+      .get(url, res => {
         res.setEncoding("utf8");
         let rawData = "";
         res.on("data", chunk => {
@@ -36,20 +26,47 @@ app.get("/matches", (request, response) => {
           try {
             const parsedData = JSON.parse(rawData);
             // console.log(parsedData);
-            response.render("showMatch", { matches: parsedData.matches });
+            // console.log(new Array("success", parsedData));
+            resolve(parsedData);
           } catch (e) {
             console.error(e.message);
-            response.send(e.message);
+            reject(e.message);
           }
         });
-      }
-    )
-    .on("error", e => {
-      console.error(`Got error: ${e.message}`);
-      response.send(e.message);
+      })
+      .on("error", e => {
+        console.error(`Got error: ${e.message}`);
+        reject(e.message);
+      });
+  });
+};
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/matches", async (req, res) => {
+  // http.get(
+  //   "http://cricapi.com/api/matches?apikey=ceTb1jjmV4PZCnj6TBjkuOgu26W2",
+  //   recData => {
+  //     console.log(recData);
+  //     res.redirect("/");
+  //   }
+  // );
+  await cricapiCall(
+    "http://cricapi.com/api/matches?apikey=ceTb1jjmV4PZCnj6TBjkuOgu26W2"
+  )
+    .then(result => {
+      // console.log(result);
+      res.render("showMatch", { matches: result.matches });
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(err);
     });
 });
 
+app.get("/match/:id", (req, res) => {});
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server started at ${port}`);
